21900117d129a58ba69894fd7e86137e752c8b08
erikacamilleri <erika.camilleri@gmail.com>
2022-10-18 14:03:22 +0200

    Minor Fix

---
+++ lessons-year-ten/src/HelloVisualStudioCode/HelloVisualStudio.java
@@ -0,0 +1,5 @@
+public class HelloVisualStudio {
+    public static void main(String[] args) {
+        System.out.println("Hello Visual Studio Code.");
+    }
+}
--- lessons-year-ten/src/HelloVisualStudioCode/HelloVisualStudioCode.java
+++
@@ -1,8 +0,0 @@
-/**
- * Lesson 1
- */
-public class HelloVisualStudioCode {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Visual Studio Code!");
-    }
-}
