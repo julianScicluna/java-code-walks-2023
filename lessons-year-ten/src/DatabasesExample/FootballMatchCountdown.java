37ee3608fceed15203641637a9142947d9101203
erikacamilleri <erika.camilleri@gmail.com>
2022-11-25 11:08:32 +0100

    Update FootballMatchCountdown.java

--- lessons-year-ten/src/DatabasesExample/FootballMatchCountdown.java
+++ lessons-year-ten/src/DatabasesExample/FootballMatchCountdown.java
@@ -4,22 +4,29 @@ import java.sql.*;
 
 public class FootballMatchCountdown {
 
-    public static String FIFA_DB = "database\\FIFAWorldCupQatar2022.mdb";
-
-	public static String TEAMS_QUALIFIED_GROUP_STAGE_SQL = "SELECT Country FROM Teams WHERE TEAMS.[Is Qualified] = TRUE";
+    public static String FIFA_DB = "jdbc:ucanaccess://database\\FIFAWorldCupQatar2022.mdb";
+	public static String TEAMS_QUALIFIED_GROUP_STAGE_SQL = "SELECT [Team ID], Country FROM Teams WHERE TEAMS.[Is Qualified] = TRUE";
     
     public static void main(String[] args) {
         try {
-			// Location of the database 
-			String url = "jdbc:ucanaccess://" + FIFA_DB;
-			// Url, Username, Pasword
-			Connection connection = DriverManager.getConnection(url);
-			System.out.println("Connected");
+			// Ask the JDBC to create a connection to Microsoft Access
+			Connection connection = DriverManager.getConnection(FIFA_DB);
+			System.out.println("*** You are connected to FIFA World Cup Qatar Microsoft Access Database ***");
+			System.out.println("\n\n");
+
+			System.out.println("Question to the Database:\nWhich country FIFA members from all teams qualified in the tournament?");
+			// Let us create the query...
 			Statement statement = connection.createStatement();
+			// Let us run it...
 			ResultSet rs = statement.executeQuery(TEAMS_QUALIFIED_GROUP_STAGE_SQL);
-			while (rs.next()) { // if we are pointing to the first result...
-				System.out.println(rs.getString("Country"));
+			
+			System.out.println("--------------------------------------------------------------------------------------");
+			
+			while (rs.next()) { // repeat until there are no more results	
+				System.out.println(rs.getString("Team ID") + " " + rs.getString("Country"));
 			}
+
+			// We do not need this resource anymore, the computer can take it back
 			connection.close();
 		} catch (SQLException e) {
 			System.err.println("Error");
