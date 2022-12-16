3e09e90998f29b1228fcb096dbe8e28eb09824ce
erikacamilleri <erika.camilleri@gmail.com>
2022-12-16 10:32:40 +0100

    Loops 2 update

--- lessons-year-ten/src/Looping2/SimpleCalculator.java
+++ lessons-year-ten/src/Looping2/SimpleCalculator.java
@@ -9,53 +9,55 @@ import Helpers.Keyboard;
 public class SimpleCalculator {
     public static void main(String[] args) {
         // Declare a variable of type char named operator
-        char operator;
+        char operator, userResponse;
 
         // Declare three double variables: number1, number2 and result
         double number1, number2, result;
+        userResponse = 'y';
+        while(userResponse == 'y') {
+            // Prompt user to enter an operator e.g.: +, -, * or /
+            System.out.println("Enter an operator e.g.: +, -, * or /");
+            operator = Keyboard.readChar();
 
-        // Prompt user to enter an operator e.g.: +, -, * or /
-        System.out.println("Enter an operator e.g.: +, -, * or /");
-        operator = Keyboard.readChar();
+            // Prompt user to enter number1 and number2
+            System.out.println("Enter number1:");
+            number1 = Keyboard.readDouble();
 
-        // Prompt user to enter number1 and number2
-        System.out.println("Enter number1:");
-        number1 = Keyboard.readDouble();
+            System.out.println("Enter number2:");
+            number2 = Keyboard.readDouble();
 
-        System.out.println("Enter number2:");
-        number2 = Keyboard.readDouble();
-
-        // Switch Case statement for operator
-        switch(operator) {
-            case '+': {
-                // Perform sum of numbers and display the result
-                result = number1 + number2; // expression
-                System.out.println("The answer is: " + result);
-                break;
+            // Switch Case statement for operator
+            switch(operator) {
+                case '+': {
+                    // Perform sum of numbers and display the result
+                    result = number1 + number2; // expression
+                    System.out.println("The answer is: " + result);
+                    break;
+                }
+                case '-': {
+                    // Perform sum of numbers and display the result
+                    result = number1 - number2; // expression
+                    System.out.println("The answer is: " + result);
+                    break;
+                }
+                case '*': {
+                    // Perform sum of numbers and display the result
+                    result = number1 * number2; // expression
+                    System.out.println("The answer is: " + result);
+                    break;
+                }
+                case '/': {
+                    // Perform sum of numbers and display the result
+                    result = number1 * number2; // expression
+                    System.out.println("The answer is: " + result);
+                    break;
+                }
+                default: {
+                    System.out.println("Error: The program does not recognise the operator.");
+                }
             }
-            case '-': {
-                // Perform sum of numbers and display the result
-                result = number1 - number2; // expression
-                System.out.println("The answer is: " + result);
-                break;
-            }
-            case '*': {
-                // Perform sum of numbers and display the result
-                result = number1 * number2; // expression
-                System.out.println("The answer is: " + result);
-                break;
-            }
-            case '/': {
-                // Perform sum of numbers and display the result
-                result = number1 * number2; // expression
-                System.out.println("The answer is: " + result);
-                break;
-            }
-            default: {
-                System.out.println("Error: The program does not recognise the operator.");
-            }
-
-            // TODO: Make changes so that the user is able to make more calculations until he wants to quit the program
+            System.out.println("Do you want to do another calculation? (y/n)");
+            userResponse = Keyboard.readChar();
         }
     }  
 }
