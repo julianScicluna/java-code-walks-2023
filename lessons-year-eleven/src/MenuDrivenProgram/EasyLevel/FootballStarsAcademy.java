ab3800e1b4fa3d60113ee62ea15e7985191c9638
erikacamilleri <erika.camilleri@gmail.com>
2023-11-16 10:24:10 +0100

    Uploaded notes on objects (refresher from last year)

--- lessons-year-eleven/src/MenuDrivenProgram/EasyLevel/FootballStarsAcademy.java
+++ lessons-year-eleven/src/MenuDrivenProgram/EasyLevel/FootballStarsAcademy.java
@@ -3,15 +3,17 @@ package MenuDrivenProgram.EasyLevel;
 import java.net.URI;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-
+import java.time.temporal.ChronoUnit;
 import java.awt.Desktop;
 
 import Helpers.Keyboard;
 
 public class FootballStarsAcademy {
     
+    // keep track of points
+    public static int points = 0;
+    
     public static void main(String[] args) {
-
         // Print out a flashy title for my app
         System.out.println("       __                                          __                    ");
         System.out.println(" _____/ /_____ ___________   ____ __________ _____/ /__  ____ ___  __  __");
@@ -49,35 +51,53 @@ public class FootballStarsAcademy {
                 dribblingExercise();
             }
             case 2: {
-
+                System.out.println("Let us start your passing exercise...");
+                try {
+                    Thread.sleep(1000);
+                } catch(Exception e) {
+                    // if the program feels like it was going to crash
+                    // it will stop the crash and try to recover
+                    System.out.println("Sorry, something went wrong... but we can continue!");
+                }
+                passesExercise();
             }
             case 3: {
-
+                System.out.println("Let us start your passing exercise...");
+                try {
+                    Thread.sleep(1000);
+                } catch(Exception e) {
+                    // if the program feels like it was going to crash
+                    // it will stop the crash and try to recover
+                    System.out.println("Sorry, something went wrong... but we can continue!");
+                }
+                runningExercise();
             }
             case 4: {
-
+                System.out.println("Keep up the good work. See you next time!");
             }
             default: {
-
+                System.out.println("The option is invalid, so we are going to quit.");
             }
         }
-        recordActivity("Dribble", "https://www.youtube.com/watch?v=jwIHc9rz7yo");
     }
 
     public static void dribblingExercise() {
-        // TODO: Code that will launch the dribbling exercise for the user
+        recordActivity("Dribble", "https://www.youtube.com/watch?v=jwIHc9rz7yo");
     }
 
     public static void passesExercise() {
-        // TODO: Code that will launch the passes exercise for the user
+        recordActivity("Passes", "https://www.youtube.com/watch?v=jwIHc9rz7yo");
     }
 
     public static void runningExercise() {
-        // TODO: Code that will launch the running exercise for the user
+        recordActivity("Running", "https://www.youtube.com/watch?v=jwIHc9rz7yo");
     }
 
     public static void recordActivity(String name, String activityUri) {
+        // get the time now
         LocalTime startTime = LocalTime.now();
+        
+        // load the activity for the user
         try 
         {
             Desktop desktop = java.awt.Desktop.getDesktop();
@@ -86,12 +106,23 @@ public class FootballStarsAcademy {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        
+        // send some feedback so that the user knows what is going on
         System.out.println(name + " Activity Launched @ " + startTime.format(DateTimeFormatter.ofPattern("HH:mm:ss")));
         System.out.print("Type 'Finish' to record activity: ");
+        
+        // wait for the user to finish
         String userPrompt = Keyboard.readString();
+        
         if (userPrompt.equals("Finish")) {
-            // TODO: calculate training duration
-            // TODO: if enough time passed, then record it, otherwise error out
+            LocalTime finishTime = LocalTime.now();
+            // calculate training duration
+            double seconds = (double) startTime.until(finishTime, ChronoUnit.SECONDS);
+            // if enough time passed, then record it, otherwise error out
+            if (seconds > 5) {
+                points = (int) (points + (seconds * 2));
+                System.out.println("You got " + points + " points");
+            }
         }
     } 
 }
