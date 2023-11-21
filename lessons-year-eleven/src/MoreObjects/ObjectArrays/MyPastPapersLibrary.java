53951f1d09a77f91706276f8e1ccef0c686feafa
erikacamilleri <erika.camilleri@gmail.com>
2023-11-21 11:21:29 +0100

    WIP Past Paper Library

--- lessons-year-eleven/src/MoreObjects/ObjectArrays/MyPastPapersLibrary.java
+++ lessons-year-eleven/src/MoreObjects/ObjectArrays/MyPastPapersLibrary.java
@@ -1,8 +1,21 @@
 package MoreObjects.ObjectArrays;
 
 public class MyPastPapersLibrary {
+
+    static PastPaper[] myLibrary = new PastPaper[20];
     
     public static void main(String[] args) {
+        
+        // TODO: Display a flashy title
+
+        // TODO: Display a menu of options
+
+        // TODO: Accept user input for choice
+
+        // TODO: A switch case statement to implement each menu item
+    }
+
+    public static void addPastPaper() {
 
     }
 }
--- lessons-year-eleven/src/MoreObjects/ObjectArrays/PastPaper.java
+++ lessons-year-eleven/src/MoreObjects/ObjectArrays/PastPaper.java
@@ -2,4 +2,12 @@ package MoreObjects.ObjectArrays;
 
 public class PastPaper {
     
+    // Data
+    String subject; // e.g., "Computer Science 0478+"
+
+    int year; // e.g., 2017
+
+    char type; // e.g., 'A' 'B'
+
+    // Methods
 }
